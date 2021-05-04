b38e9e7d0c72efebb7043ce39e90075a84b389da
Frank Kuo <frankkuo@protonmail.com>
2021-05-04 23:39:50 +0800

    Update p5.c

--- HW2/p5.c
+++ HW2/p5.c
@@ -8,7 +8,7 @@ https://en.wikipedia.org/wiki/Fenwick_tree
 #include <stdlib.h>
 #include "generator.h"
 
-#define MAX_CORD 1048576
+#define MAX_CORD 1048575
 #define MIN_CORD -1048576
 #define OFFSET 1048576+1
 
@@ -31,9 +31,14 @@ int lowbit(int x){
 int main() {
     generator.init();
     int t = generator.getT();
+    // int t=1;
     while (t--) {
         int n, *p, *q, *r;
         generator.getData(&n, &p, &q, &r);
+        // int n = 5;
+        // int p[5] = {0, 0, 0, 0, 1};
+        // int q[5] = {1, 3, 5, 7, 9};
+        // int r[5] = {2, 4, 6, 8, 10};
 
         // Sort triangles by p        
         tri** paths = malloc(n*sizeof(tri*));
@@ -54,10 +59,13 @@ int main() {
         int sep = 0;
         // Use a Binary Index Tree to store r values
         int* BIT = malloc((MAX_CORD-MIN_CORD+1+1)*sizeof(int));
+        for (int i=0; i<MAX_CORD-MIN_CORD+1+1; i++) {
+            BIT[i] = 0;
+        }
         int tree_size = 0;
         int* r_hold = malloc(n*sizeof(int));
         int hold_size = 0;
-        int old_p = MIN_CORD-1;
+        int old_p = MIN_CORD-100;
         for (int i=0; i<n; i++) {
             int p_i = paths[i]->p;
             int q_i = paths[i]->q;
@@ -66,7 +74,8 @@ int main() {
             if (p_i != old_p) {
                 while (hold_size) {
                     hold_size--;
-                    for (int j=r_hold[hold_size]+OFFSET; j<= MAX_CORD-MIN_CORD+1; j+=lowbit(j)) {
+                    for (int j=r_hold[hold_size]+OFFSET; j<= MAX_CORD-MIN_CORD+1;
+                            j+=lowbit(j)) {
                         BIT[j]++;
                     }
                 }
@@ -74,14 +83,16 @@ int main() {
             }
             // Query sum from MIN_CORD to q_i
             for (int j=q_i+OFFSET-1; j>0; j-=lowbit(j)) {
-                if (BIT[j])
-                // printf("%d ", i);
                 sep += BIT[j];
             }
-            // printf("\nsep=%d\n", sep);
             // Add r_i to waiting line
             r_hold[hold_size] = r_i;
             hold_size++;
+            // printf("%d\n", sep);
+            // for (int k=0; k<hold_size; k++) {
+            //     printf("%d ", r_hold[k]);
+            // }
+            // printf("\n");
         }
 
         int ans = n*(n-1)/2 - sep;
